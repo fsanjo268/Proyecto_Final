0f0875675f6e9644f5813a1b85f17ec7b591ec1b
MafioP <42208309+MafioP@users.noreply.github.com>
2019-12-11 13:45:55 +0100

    imprimir letras en matriz, en vez de matriz de letras

--- src/Proyecto_final.java
+++ src/Proyecto_final.java
@@ -2,43 +2,9 @@ import java.util.Scanner;
 
 public class Proyecto_final {
 	
-//método matriz normal
-	public static void imprimirMat (char [][] matriz) {
-
-		System.out.println(" |0|1|2|3|4|5|6|7|8|9|");
-		for(int i = 0; i<10; i++) {
-			System.out.print( i + "|" + "");
-			
-			for(int j = 0; j<10; j++) 
-				System.out.print (matriz[i][j] + "|");
-			
-			System.out.println("" + i);		
-		
-		} 
-	    
-		System.out.println(" |0|1|2|3|4|5|6|7|8|9|");
-	}
-
-//metodo matriz pruebas
-	public static void imprimirMatprueba (char [][] matrizPruebas) {
-
-		System.out.println(" |0|1|2|3|4|5|6|7|8|9|");
-		for(int i = 0; i<10; i++) {
-			System.out.print( i + "|" + "");
-			
-			for(int j = 0; j<10; j++) 
-				System.out.print (matrizPruebas[i][j] + "|");
-			
-			System.out.println("" + i);		
-		
-		} 
-	    
-		System.out.println(" |0|1|2|3|4|5|6|7|8|9|");
-	}
-
-
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner (System.in);
+		char[][] letras;
+ 		Scanner teclado = new Scanner (System.in);
 		//variables
 			char m =0 , car;
 			String cadena = "p";
@@ -47,8 +13,7 @@ public class Proyecto_final {
 			String [] palabrasPruebas = {"AAA", "AAAAAA", "BBBBBBBBB", "CCCCC", "DDDDD", "EEEEE", "FFFFF", "GGGGG", "HHHHH", "IIIII", "JJJJJ"};
 			String [] palabras = {"SOLENOIDE", "ORANGUTAN", "RATON", "SOLEDAD", "ARBOL", "QUESO", "ORDENADOR", "FAROLA", "PAPEL", "CASA", "CARABINA"};
 		//creacion de la matriz 10x10 junto a las palabras
-			char [][] matrizPruebas = NuevaMatriz.nuevaMatriz (10, 10, palabrasPruebas);
-			char [][] matriz = NuevaMatriz.nuevaMatriz (10, 10, palabras);
+
 
 		//Eleccion del modo de juego
 			System.out.println("Eliga el modo de juego, pruebas escriba P, para el modo normal escriba otra entrada");
@@ -65,15 +30,35 @@ public class Proyecto_final {
 			
 		   
 			if ((car=='p') || (car=='P')) 
-				imprimirMat (matriz);//imprime el metodo de la matriz normal
+				letras = imprimirMat (palabrasPruebas);//imprime el metodo de la matriz normal
 			else 
 				System.out.println("true");
-				imprimirMatprueba (matrizPruebas);//imprime el metoso de la matriz pruebas
+				letras = imprimirMat (palabras);//imprime el metoso de la matriz pruebas
 			
 			
 		
 			
 	}
+	
+//método matriz 
+	public static char [][] imprimirMat (String[] palabras) {
+		char [][] matriz = NuevaMatriz.nuevaMatriz (10, 10, palabras);
+
+		System.out.println(" |0|1|2|3|4|5|6|7|8|9|");
+		for(int i = 0; i<10; i++) {
+			System.out.print( i + "|" + "");
+			
+			for(int j = 0; j<10; j++) 
+				System.out.print (matriz[i][j] + "|");
+			
+			System.out.println("" + i);		
+		
+		} 
+	    
+		System.out.println(" |0|1|2|3|4|5|6|7|8|9|");
+		return matriz;
+	}
+
 }		
 
 
