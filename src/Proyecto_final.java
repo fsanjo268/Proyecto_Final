67a0cc1b34e5edf849080e2ee75057cdc329d150
fsanjo268 <fsanjo268@gmail.com>
2019-12-12 20:33:26 +0100

    Adición de lectura de coordenadas

--- src/Proyecto_final.java
+++ src/Proyecto_final.java
@@ -8,7 +8,9 @@ public class Proyecto_final {
 		//variables
 			char m =0 , car;
 			String cadena = "p";
-		    
+			char [] valor1 = new char[4];
+	    	int x, y, lo;
+	    	char orientacion;
 		//creacion de la lista de palabras de prueba y normal
 			String [] palabrasPruebas = {"AAA", "AAAAAA", "BBBBBBBBB", "CCCCC", "DDDDD", "EEEEE", "FFFFF", "GGGGG", "HHHHH", "IIIII", "JJJJJ"};
 			String [] palabras = {"SOLENOIDE", "ORANGUTAN", "RATON", "SOLEDAD", "ARBOL", "QUESO", "ORDENADOR", "FAROLA", "PAPEL", "CASA", "CARABINA"};
@@ -31,14 +33,30 @@ public class Proyecto_final {
 			if ((car=='p') || (car=='P')) 
 				letras = imprimirMat (palabrasPruebas);//imprime el metodo de la matriz normal
 			else 
-				letras = imprimirMat (palabras);//imprime el metoso de la matriz pruebas			
+				letras = imprimirMat (palabras);//imprime el metoso de la matriz pruebas		
+			
+			/* bucle para que te pida coordenadas hasta que se acaben las palabras*/
+			while(true) {
+			System.out.println("Inserte coordenadas de fila = x, columna = y, dirección(Norte = N, Sur = S, Este = E, Oeste = W) y longitud de la palabra");
+			String coordenadas = teclado.next();
+			valor1 = leerCoordenadas (coordenadas);/* el metodo ( return valor2) se iguala al valor1*/
+			x = Character.getNumericValue(valor1[0]);/* convierte un character a un int*/
+			y = Character.getNumericValue(valor1[1]);
+			orientacion = valor1[2];
+			lo = Character.getNumericValue(valor1[3]);
+			System.out.println("fila " + x + " columna " + y + " orientacion " + orientacion + " longitud " + lo);
+			}
 	}
-	/*int coordenadas = 4;
-	switch (coordenadas) {
 	
-	}*/
-	 
+	/* metodo para leer las coordenadas*/
+	public static char [] leerCoordenadas (String coordenadas){
+		char [] valor2 = new char[4];/*matriz de 4 ranuras que lea los 4 caracteres*/
 	
+		for(int i = 0; i<coordenadas.length(); i++) { /*bucle for para poder leer las 4 posiciones de coordenadas y las almacene*/
+			valor2[i] = coordenadas.charAt(i);
+		}
+		return valor2;
+	}
 	
 //metodo matriz 
 	public static char [][] imprimirMat (String[] palabras) {
